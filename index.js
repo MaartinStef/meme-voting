4a0f202696cd84d4b7e1f048d546e0d393d74e6c
MaartinStef <mato.stefko@gmail.com>
2019-04-03 21:02:24 +0200

    Added meme voting to with blockchain

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var memesLength = 0;
 //         {"creatorName":"Carol","memeUrl":"https://pbs.twimg.com/media/DeHbsSAU0AAbQAq.jpg","votes":18,"index":3,"rank":3}
 //     ]
 
-// Toto treba abz mustache renderov časť ktorá je v index.html 
+// Toto treba abz mustache renderov časť ktorá je v index.html,zobrazi aj memečka 
 function renderMemes() {
 memeArray = memeArray.sort(function(a,b){return b.votes-a.votes})
 var template = $('#template').html();
@@ -25,6 +25,15 @@ async function callStatic(func, args, types){
     const decodedGet = await client.contractDecodeData(types, calledGet.result.returnValue).catch(e=>console.error(e));
     
     return decodedGet;
+}
+
+
+async function contractCall(func, args, value, types) {
+    const calledSet = await client.contractCall(contractAddress, 'sophia-address', contractAddress, func, {args,options:{amount:value}}).catch(async e=>{
+        const decodedError = await client.contractDecodeData(types,e,returnValue).catch(e => console.error(e));
+    });
+
+    return
 }
 
 
@@ -54,18 +63,27 @@ window.addEventListener('load',async () => {
 // Koniec
 // Zoberie tu hodnotu hlasu(ae) ktoru prave klika uzivatel, najde jej memem id na ktore klika  a prida hodnotu do memeArray
 jQuery("#memeBody").on("click", ".voteBtn", async function(event){
+    $("#loader").show();   
     const value = $(this).siblings('input').val();
     const dataIndex = event.target.id;
+
+    await contractCall('voteMeme',`(${dataIndex})`,value,'(int)');
+
     const foundIndex = memeArray.findIndex(meme => meme.index == dataIndex);
     memeArray[foundIndex].votes += parseInt(value, 10);
+    
     renderMemes();
-
-
+    
+    $("#loader").hide();
 });
 // na Register gombik klik vzkona async funkciu najde tu hodnotu regName a regUrl na ktoru klikam, priradi ich do premennych,pushne ich do memeArray a pripocite 1 k deterajsej dlzke memesArray{ co bude index novoregistrovaneho meme}
 $('#registerBtn').click(async function(){
-    var name = ($('#regName').val()),
-        url = ($('#regUrl').val());
+    $("#loader").show();
+    
+    const name = ($('#regName').val()),
+          url = ($('#regUrl').val());
+
+    await contractCall('registerMeme', ("${url}","${name}"),0,'(int)');
 
     memeArray.push({
         creatorName : name ,
@@ -76,6 +94,7 @@ $('#registerBtn').click(async function(){
 
     })
     renderMemes();
+    $("#loader").hide();
 
 })
 
